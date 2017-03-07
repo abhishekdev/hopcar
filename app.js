c9c2ed3e731ffc30f339e55a95e9ccd252faa939
Abhishek Dev <abhishek.office@yahoo.com>
2017-03-07 08:40:34 -0500

    add express route to point to index.html for all cases

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
+app.get('*', (req, res) => {
+    // All requests fallback to the index.html SPA
+    res.sendfile(path.join(__dirname, 'dist/index.html'));
+});
+
 app.listen(app.get('port'), () => console.log('Node server is running on port', app.get('port')));
