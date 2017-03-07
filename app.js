fe387385f4ef2266ce93541456ebf344572106b0
Abhishek Dev <abhishek.office@yahoo.com>
2017-03-07 12:39:38 -0500

    :lock: Don not serve sourcemaps in production environment
    - Also fix deprecated warning for ExpressJS (sendfile to sendFile)

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const env = process.env.NODE_ENV || 'development';
 
 app.set('port', (process.env.PORT || 5000));
+
+// Disable source maps in production
+if (env === 'production') {
+    app.use('/*.map', (req, res, next) => {
+        res.status(404).send('Sorry cannot find that!');
+    });
+}
 
 app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    // All requests fallback to the index.html SPA
-    res.sendfile(path.join(__dirname, 'dist/index.html'));
+    // All requests fallback to the index.html (viz. a Single Page Application)
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(app.get('port'), () => console.log('Node server is running on port', app.get('port')));
+app.listen(app.get('port'), () => console.log('Hopcar server is running on port', app.get('port')));
